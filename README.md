dd33d184f383c1392e28c4e35e6de9a97c767f6c
Ali Almasi <A710almasi@gmail.com>
2023-11-07 02:06:35 +0330

    2.3.0

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-<h1>My personal <i>npx</i> contact card</h1>
+<h1>My personal <i>npx</i> card</h1>
 <h2>Run <code>npx alialmasi</code> on your terminal.</h2>
-<blockquote>Tip: Install <a href="https://nodejs.org/">Node.js</a>, if you haven't already.</blockquote>
-<img src="https://github.com/AliAlmasi/npx-alialmasi/assets/87055424/ef7553d0-5cfe-45a8-b623-6ae365d18da1">
+<a href="https://github.com/AliAlmasi/npx-alialmasi"><img src="./screenshot.jpg"></a>
 <h3><a href="https://github.com/AliAlmasi/npx-alialmasi">Link to GitHub repository</a></h3>
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 import boxen from "boxen";
 import chalk from "chalk";
+import clear from "clear";
 
 const boxenOptions = {
   padding: 2,
@@ -9,8 +10,7 @@ const boxenOptions = {
   title: "Ali Almasi",
   titleAlignment: "center",
   borderStyle: "round",
-  borderColor: "#fff",
-  float: "center",
+  borderColor: "#2188ff",
   margin: 3
 };
 
@@ -19,14 +19,15 @@ const intro = chalk.bold(
 );
 
 const links = [
-  { name: chalk.black.bgWhite("My webpage"), space: "  ", url: ("https://") + chalk.bold("al1almasi.ir") },
-  { name: chalk.magenta.bgBlack("GitHub"), space: "      ", url: ("https://") + chalk.bold("github.al1almasi.ir") },
-  { name: chalk.white.bgBlue("LinkedIn"), space: "    ", url: ("https://") + chalk.bold("linkedin.al1almasi.ir") },
-  { name: chalk.white.bgMagenta("Mastodon"), space: "    ", url: ("https://") + chalk.bold("mastodon.al1almasi.ir") },
-  { name: chalk.blue.bgBlack("My notebook"), space: " ", url: ("https://") + chalk.bold("note.al1almasi.ir") },
-  { name: chalk.white.bgBlack("My blog"), space: "     ", url: ("https://") + chalk.bold("blog.al1almasi.ir") }
+  { name: chalk.black.bgWhite("My webpage"), space: "  ", url: chalk.dim("https://") + chalk.bold("al1almasi.ir") },
+  { name: chalk.magenta.bgBlack("GitHub"), space: "      ", url: chalk.dim("https://") + chalk.bold("github.al1almasi.ir") },
+  { name: chalk.white.bgBlue("LinkedIn"), space: "    ", url: chalk.dim("https://") + chalk.bold("linkedin.al1almasi.ir") },
+  { name: chalk.white.bgMagenta("Mastodon"), space: "    ", url: chalk.dim("https://") + chalk.bold("mastodon.al1almasi.ir") },
+  { name: chalk.blue.bgBlack("My notebook"), space: " ", url: chalk.dim("https://") + chalk.bold("note.al1almasi.ir") },
+  { name: chalk.white.bgBlack("My blog"), space: "     ", url: chalk.dim("https://") + chalk.bold("blog.al1almasi.ir") }
 ];
 
 const linkList = links.map((link) => `${link.name} ${link.space} ${link.url}`).join("\n");
 
+clear();
 console.log("\n" + boxen(intro + linkList, boxenOptions) + "\n");
